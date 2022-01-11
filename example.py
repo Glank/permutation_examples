57ef53d9240b885cf08053db63bb7452bf20b166
Ernest Kirstein <glank314@gmail.com>
2022-01-11 12:44:32 -0500

    Adding example gifs.

--- example.py
+++ example.py
@@ -5,14 +5,14 @@ if __name__=='__main__':
     [2, 3, 4, 1, 0],
   ]
   imgs = animate_permutations(permutations, ["red", "blue", "yellow", "green", "purple"], ["1", "2", "3", "4", "5"], (500, 250), 30, 50, 20)
-  imgs[0].save('example_1.gif', save_all=True, append_images=imgs[1:], loop=0, duration=50)
+  imgs[0].save('examples/example_1.gif', save_all=True, append_images=imgs[1:], loop=0, duration=50)
 
   permutations = [
     [0, 3, 2, 1, 4],
     [2, 1, 4, 3, 0],
   ]
   imgs = animate_permutations(permutations, ["red", "blue", "yellow", "green", "purple"], ["1", "2", "3", "4", "5"], (500, 250), 30, 50, 20)
-  imgs[0].save('example_2.gif', save_all=True, append_images=imgs[1:], loop=0, duration=50)
+  imgs[0].save('examples/example_2.gif', save_all=True, append_images=imgs[1:], loop=0, duration=50)
 
   permutations = [
     [0, 3, 2, 1, 4],
@@ -20,4 +20,4 @@ if __name__=='__main__':
     [2, 1, 0, 3, 4],
   ]
   imgs = animate_permutations(permutations, ["red", "blue", "yellow", "green", "purple"], ["1", "2", "3", "4", "5"], (500, 250), 30, 50, 20)
-  imgs[0].save('example_3.gif', save_all=True, append_images=imgs[1:], loop=0, duration=50)
+  imgs[0].save('examples/example_3.gif', save_all=True, append_images=imgs[1:], loop=0, duration=50)
